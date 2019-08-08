b59676df64ddb6de9c428b24b281828d4298d72d
Jose Pablo Ramirez Mayorga <jota_ramirezm@outlook.com>
2019-08-07 19:48:48 -0600

    Images

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,60 +5,65 @@ const Header = () => {
     <div class="card-group ">
       <div class="card">
         <img
-          src="https://images.squarespace-cdn.com/content/v1/529c0da8e4b0e42a6a7d50e5/1391996878444-PQRNZJJS3OL971J8VLI4/ke17ZwdGBToddI8pDm48kCpX2mwG9slVUzQCwhOMrQF7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UVDXM9yQ8sG6x3COIEUaadqpk9XPubC0H4MH9Az_c7nPqIjSxZ2rgD2_Fw9U6DWfsg/Tortuguero-Canals-Sunrise-Tortuguero-amanecer-canales.jpg?format=2500w"
+          src="https://farm4.staticflickr.com/3153/2399273202_165f20acf8_b.jpg"
           class="card-img-top"
           alt="..."
         />
         <div class="card-body">
-          <h5 class="card-title">Tortuguero National Park</h5>
+          <h5 class="card-title">Costa Rica - Vista Aerea</h5>
           <p class="card-text">
-            Parque Nacional Tortuguero is a protected wilderness area on Costa Rica's northern
-            Caribbean coast. Its beaches are famous nesting grounds for sea turtles, including
-            endangered green turtles. The park's freshwater creeks and lagoons, which can be
-            navigated by boat or canoe, shelter spectacled caimans and river turtles. The
-            surrounding dense rainforest is also rich with wildlife, from monkeys to many bird
-            species.
+            Costa Rica, denominada oficialmente República de Costa Rica, es una nación soberana,
+            organizada como una república presidencialista unitaria compuesta por 7 provincias.
+            Ubicada en América Central, posee un territorio con un área total de 51 100 km². Limita
+            con Nicaragua al norte, el mar Caribe al este, Panamá al sureste y el océano Pacífico al
+            oeste. En cuanto a los bordes marítimos, colinda con Panamá, Nicaragua, Colombia y
+            Ecuador.7​8​ Cuenta con 5 003 393 habitantes según su última proyección demográfica.9​
+            Su capital, centro político y económico es San José, y su idioma oficial es el español.1
           </p>
           <p class="card-text">
-            <small class="text-muted">Photo by Sergio Pucci</small>
+            <small class="text-muted">Fotografia por emills1</small>
           </p>
         </div>
       </div>
       <div class="card">
         <img
-          src="https://images.squarespace-cdn.com/content/v1/529c0da8e4b0e42a6a7d50e5/1391996852845-BFO9HUPLYZZYSHGERO76/ke17ZwdGBToddI8pDm48kCpX2mwG9slVUzQCwhOMrQF7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UVDXM9yQ8sG6x3COIEUaadqpk9XPubC0H4MH9Az_c7nPqIjSxZ2rgD2_Fw9U6DWfsg/Stanta-Rosa-Playa+Naranjo.jpg?format=2500w"
+          src="https://live.staticflickr.com/7902/45934582255_88b08ff638_b.jpg"
           class="card-img-top"
           alt="..."
         />
         <div class="card-body">
-          <h5 class="card-title">Naranjo beach</h5>
+          <h5 class="card-title">Fauna de Costa Rica</h5>
           <p class="card-text">
-            Playa Naranjo, also known as Witches Rock or Roca Bruja in Spanish, is located in
-            Guanacaste Costa Rica. The beach is extremely popular among surfers and is known for
-            amazing scenery. The remote location makes it somewhat harder to reach and is therefore
-            reserved for the serious and dedicated surfer rather than the beginner.
+            Costa Rica cuenta con una enorme variedad de vida silvestre, debido en gran parte a su
+            posición geográfica entre los continentes del Norte y América del Sur, su clima
+            neotropical y su amplia variedad de hábitats. Costa Rica es hogar de más de 500.000
+            especies, que representa casi el 4% de las especies totales estimado en todo el mundo,
+            haciendo de Costa Rica uno de los 20 países con la más alta biodiversidad en el mundo.
+            De estas 500.000 especies, un poco más de 300.000 son insectos.5​
           </p>
           <p class="card-text">
-            <small class="text-muted">Photo by Juan J Pucci</small>
+            <small class="text-muted">Fotografia por Scuba Chris</small>
           </p>
         </div>
       </div>
       <div class="card">
         <img
-          src="https://images.squarespace-cdn.com/content/v1/529c0da8e4b0e42a6a7d50e5/1391996594427-LHZAABHXCBSVFWFHAR4F/ke17ZwdGBToddI8pDm48kFWxnDtCdRm2WA9rXcwtIYR7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UcTSrQkGwCGRqSxozz07hWZrYGYYH8sg4qn8Lpf9k1pYMHPsat2_S1jaQY3SwdyaXg/Arenal-volcano.jpg?format=2500w"
+          src="https://live.staticflickr.com/4131/5037262539_9cd7efa9e9_b.jpg"
           class="card-img-top"
           alt="..."
         />
         <div class="card-body">
-          <h5 class="card-title">Arenal volcano cone from Arenal Lake</h5>
+          <h5 class="card-title">Lago Arenal</h5>
           <p class="card-text">
-            Arenal Volcano is a dormant andesitic stratovolcano in north-western Costa Rica around
-            90 km northwest of San José, in the province of Alajuela, canton of San Carlos, and
-            district of La Fortuna. The Arenal volcano measures at least 1,633 metres high. It is
-            conically shaped with a crater 140 metres in diameter.
+            El lago Arenal es un embalse artificial situado en Costa Rica formado tras la
+            construcción de una presa en 1979 aprovechando la existencia de un pequeño lago del
+            mismo nombre. Se localiza cerca del volcán Arenal , Parque Nacional Volcán Tenorio y del
+            bosque nuboso de Monteverde en las coordenadas 10.53, -84.93. Las ciudades de Tilarán y
+            La Fortuna, en las provincias de Guanacaste y Alajuela respectivamente, son las más
+            grandes e importantes en sus cercanías.
           </p>
           <p class="card-text">
-            <small class="text-muted">Photo by Juan J Pucci</small>
+            <small class="text-muted">Fotografia por Pato Novoa</small>
           </p>
         </div>
       </div>
