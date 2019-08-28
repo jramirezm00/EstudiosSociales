33b8bd4a6d786684f74bb8987652b81fe8d900de
Jose Pablo Ramirez Mayorga <jota_ramirezm@outlook.com>
2019-08-27 23:37:11 -0600

    game menu

--- src/components/gameMenu.jsx
+++ src/components/gameMenu.jsx
@@ -3,27 +3,58 @@ import { Link } from 'react-router-dom';
 
 const Menu = props => {
   return (
-    <main className="container">
+    <main
+      className="container"
+      style={{ backgroundImage: 'url(' + require('../images/monsters.png') + ')' }}
+    >
       <div>
-        <h1 className="text-center">Menu de Juegos</h1>
+        <h1 className="text-center">
+          <i class="fa fa-grav" aria-hidden="true" />
+          Menú de Juegos
+          <i class="fa fa-grav" aria-hidden="true" />
+        </h1>
         <hr className="style1" />
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
         <ul
           className="list-group list-group-horizontal-lg  d-flex align-items-center justify-content-center"
           style={{ cursor: 'pointer' }}
         >
-          <li className="list-group-item ">
+          <li className="list-group-item list-group-item-info ">
             <Link to="/">Home</Link>
           </li>
-          <li className="list-group-item ">
+          <li className="list-group-item list-group-item-info">
             <Link to="/crossword">Crucigrama</Link>
           </li>
-          <li className="list-group-item ">
+          <li className="list-group-item list-group-item-info">
             <Link to="/question1">Pregunta1</Link>
           </li>
-          <li className="list-group-item ">
+          <li className="list-group-item list-group-item-info">
             <Link to="/question2">Pregunta2</Link>
           </li>
         </ul>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
       </div>
     </main>
   );
